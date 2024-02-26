7d69c027151fe6b61fd17ed472da5a20ce406ff1
vishaljha949 <vishaljha949@gmail.com>
2024-02-26 18:50:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import HeroSection from "./assets/components/Hero";
+import Navigation from "./assets/components/Navigation";
+const App = () => {
+  return (
+    <div>
+      <Navigation />
+      <HeroSection />
+      
+      
+    </div>
+  );
+};
+
+export default App;
